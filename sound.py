7d5fe1fbb3c7a06e664db36bdaa94cc19663e2fb
Berserk007 <la.tribu.des.berserk@gmail.com>
2024-11-03 18:46:56 -0500

    relative paths

--- sound.py
+++ sound.py
@@ -1,6 +1,8 @@
 from mutagen.mp3 import MP3
+import pathlib
+
 
 class Sound:
     def __init__(self, filename: str):
         self.name = filename
-        self.length = MP3(fr"C:\Users\Dorno\Desktop\Coding projects\Python\Soundboard\sounds\{self.name}").info.length
+        self.length = MP3(fr"{pathlib.Path().resolve()}\Sounds\{self.name}").info.length
